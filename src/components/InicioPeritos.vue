891d8d812c40b6f5b7da005151902493347aa4e4
Talaia08 <a104061@uminho.pt>
2025-05-31 19:53:07 +0100

    teste 7.4

--- src/components/InicioPeritos.vue
+++ src/components/InicioPeritos.vue
@@ -98,7 +98,7 @@ export default {
     };
   },
   mounted() {
-    const auditorias = JSON.parse(localStorage.getItem('ocorrencias')) || [];
+    const auditorias = JSON.parse(localStorage.getItem('auditorias')) || [];
     const userEmail = localStorage.getItem('email');
 
     // Só mostra ocorrências atribuídas ao perito autenticado
